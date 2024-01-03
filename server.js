9066f6c9e5f1073b5bb6e2718d5b9015b0951496
bsaglimb <bsaglimb>
2024-01-03 00:00:13 -0500

    files not working

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const app = express();
-
 const allNotes = require('./miniature-eureka/Develop/db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,19 +10,23 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-//API Routes
+//HTML Routes
 app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
+//Home
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//Notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+
+//Wildcard Route
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
