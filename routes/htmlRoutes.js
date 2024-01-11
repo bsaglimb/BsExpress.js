91feced43d66fe6cfd3c01c05f55c462cbdcb54b
bsaglimb <bsaglimb>
2024-01-11 01:42:44 -0500

    changing around the htmlRoutes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,25 @@
 const path = require('path');
-const express = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-//HTML Routes
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+// API Route
+router.get('/api/notes', (req, res) => {
+  res.send('API Notes Page');
 });
 
-//Home
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+// HTML Routes
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-//Notes
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+// Home Route
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-
-//Wildcard Route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+// Wildcard Route
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
